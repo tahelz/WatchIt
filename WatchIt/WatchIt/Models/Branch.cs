2ab9ee7175371f1b6f6144f1ed9d5e9e65de66ad
YardenMarkus <markusyarden@gmail.com>
2019-07-13 14:59:12 +0300

    Entities (movie,branch,customer,genre,director)

---
+++ WatchIt/WatchIt/Models/Branch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WatchIt.Models
+{
+    public class Branch
+    {
+        public int BranchID { get; set; }
+
+        [Display(Name = "Branch Name")]
+        [Required(ErrorMessage = "Required field")]
+        [StringLength(60, MinimumLength = 2)]
+        public string BranchName { get; set; }
+
+        [Display(Name = "City")]
+        [Required(ErrorMessage = "Required field")]
+        [StringLength(60, MinimumLength = 2)]
+        public string BranchCity { get; set; }
+
+        [Display(Name = "Address")]
+        [Required(ErrorMessage = "Required field")]
+        [StringLength(60, MinimumLength = 2)]
+        public string BranchStreet { get; set; }
+
+        public string DisplayName
+        {
+            get
+            {
+                return this.BranchName + " (" + this.BranchCity + " - " + this.BranchStreet + ")";
+            }
+        }
+
+        [Display(Name = "Phone Number")]
+        [RegularExpression(@"0\d-\d{7}", ErrorMessage = "Not a valid phone number")]
+        public string BranchsPhoneNumber { get; set; }
+    }
+}
---
+++ WatchIt/WatchIt/Models/Director.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WatchIt.Models
+{
+    public class Director
+    {
+        [Key]
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        [StringLength(60, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Description")]
+        [MaxLength(500)]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        [Display(Name = "Number of Nominate")]
+        [Range(0, int.MaxValue)]
+        public int NominatedNum { get; set; }
+
+        [DataType(DataType.Upload)]
+        public string Image { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Birth Date")]
+        public DateTime BirthDate { get; set; }
+    }
+}
